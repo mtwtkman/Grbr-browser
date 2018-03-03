abc61b999c3bac57fadb4f2338b97f920a62a15e
mtwtkman <punipuniomochi@gmail.com>
2018-03-04 00:58:34 +0900

    maybe improve installer script

--- windowsInstaller.js
+++ windowsInstaller.js
@@ -1,11 +1,26 @@
 const installer = require('electron-winstaller');
+const fs = require('fs');
+const path = require('path');
+
+const src = './Grbr-win32-x64/';
+
+const removeRec = name => {
+    if (fs.statSync(name).isDirectory()) {
+        fs.readdirSync(name).forEach(x => removeRec(path.join(name, x)))
+        fs.rmdirSync(name);
+    } else {
+        fs.unlinkSync(name);
+    }
+}
 
 installer.createWindowsInstaller({
-    appDirectory: './Grbr-win32-x64/',
+    appDirectory: src,
     outputDirectory: './Grbr-win-x64/',
     authors: 'mtwtkman',
     description: 'Browser for Granbluefantasy',
     exe: 'Grbr.exe',
 })
-.then(() => console.log('done'))
+.then(() => {
+    removeRec(src);
+})
 .catch(e => console.log(e.message));
